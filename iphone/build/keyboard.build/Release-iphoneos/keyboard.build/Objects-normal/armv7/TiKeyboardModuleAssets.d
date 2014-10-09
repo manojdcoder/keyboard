dc1c31c01b65ae635ec35ccd94d49bc651209966
mano-mykingdom <mano.mykingdom@gmail.com>
2014-10-09 14:42:11 +0530

    published iPhone - 1.0

--- iphone/build/keyboard.build/Release-iphoneos/keyboard.build/Objects-normal/armv7/TiKeyboardModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/MyKingdom/Documents/Titanium_Studio_Workspace/keyboard/iphone/Classes/TiKeyboardModuleAssets.m \
-  /Users/MyKingdom/Documents/Titanium_Studio_Workspace/keyboard/iphone/Classes/TiKeyboardModuleAssets.h
